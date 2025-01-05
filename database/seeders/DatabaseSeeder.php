7da23d74c9103e8ff8b44960039b7165716c3eff
nextgenbite <iliusrahman@gmail.com>
2025-01-05 16:56:51 +0600

    yml is updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory()->create();
-        $this->call(JsonDataSeeder::class);
+        // $this->call(JsonDataSeeder::class);
 
     }
 }
